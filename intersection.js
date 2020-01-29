7dcec300f21849fbe23353ae3d091f6be2126b16
danWagar <dandwagar@gmail.com>
2020-01-28 19:57:56 -0800

    add newest proj

--- intersection.js
+++ intersection.js
@@ -1,26 +1,22 @@
 'use strict';
 
 function init() {
-  //let gradient = document.querySelector('.gradientBackground');
-  let timeline = document.querySelector('.timelineCover');
-  let screenWidth = window.innerWidth;
-  let threshold;
-
-  if (screenWidth < 800) threshold = 0.5;
-  else threshold = 0.5;
+  let threshold = 0.5;
+  let projectThreshold = 0.2;
 
   let options = {
-    root: document.querySelector('#scrollArea'),
+    //root: document.querySelector('#scrollArea'),
     rootMargin: '0px',
     threshold: threshold
   };
 
-  let callback = entries => {
-    //console.dir(entries);
-    //console.dir(options);
-    entries.forEach(entry => {
-      //console.dir(threshold);
+  let projectOptions = {
+    rootMargin: '0px',
+    threshold: projectThreshold
+  };
 
+  let callback = entries => {
+    entries.forEach(entry => {
       if (entry.target === targetHome && entry.isIntersecting) {
         const links = document.querySelectorAll('.navLink');
         links.forEach(link => {
@@ -51,14 +47,16 @@ function init() {
   };
 
   let observer = new IntersectionObserver(callback, options);
+  let projectObserver = new IntersectionObserver(callback, projectOptions);
+
   let targetHome = document.querySelector('#home');
   let targetAbout = document.querySelector('#about');
-  let targetProjects = document.querySelector('#projects');
+  let targetProjects = document.querySelector('#projectsObserve');
   let targetContact = document.querySelector('#contact');
-  //IntersectionObserver.threshold(0);
+
   observer.observe(targetHome);
   observer.observe(targetAbout);
-  observer.observe(targetProjects);
+  projectObserver.observe(targetProjects);
   observer.observe(targetContact);
 }
 
